afc602a4625e6ee83d84717f0bc248fcce15161e
Andrei Regiani <andrei.cpp@gmail.com>
2017-06-07 00:53:07 +0300

    Made Solution-1 compatible with both Python 2 & 3 on an unified script. The extra code didn't affect the performance at all

--- round-1/solution-1.py
+++ round-1/solution-1.py
@@ -1,4 +1,8 @@
-from itertools import chain, zip_longest
+from itertools import chain
+try:
+    from itertools import zip_longest  # Python 3
+except ImportError:
+    from itertools import izip_longest as zip_longest  # Python 2
 
 for i in range(10000000):
     s1 = 'abacate' + str(i)
