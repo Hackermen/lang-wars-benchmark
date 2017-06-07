45aa442d4bde4ba26f9c0c90cd7f9f6d60ac5d69
Andrei Regiani <andrei.cpp@gmail.com>
2017-06-07 20:50:46 +0300

    Improvements on Python solution, 20% better performance

--- round-1/solution-2.py
+++ round-1/solution-2.py
@@ -1,17 +1,16 @@
 for i in range(10000000):
-    s1 = 'abacate' + str(i)
-    s2 = 'palmeiras' + str(i)
+    i_str = str(i)
+    s1 = 'abacate' + i_str
+    s2 = 'palmeiras' + i_str
     result = ''
     x = 0
     if len(s1) < len(s2):
         while x < len(s1):
-            result += s1[x]
-            result += s2[x]
+            result += s1[x] + s2[x]
             x += 1
         result += s2[x:]
     else:
         while x < len(s2):
-            result += s1[x]
-            result += s2[x]
+            result += s1[x] + s2[x]
             x += 1
         result += s1[x:]
