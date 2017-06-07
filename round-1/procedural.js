68a7c253d652a2351c2aea68d8b5268e895007ff
unknown <olixis@gmail.com>
2017-06-06 22:46:02 -0300

    solução procedural js

---
+++ round-1/procedural.js
@@ -0,0 +1,26 @@
+console.time('perf');
+
+for (let i = 10000000; i--;) {
+    let s1 = 'abacate' + i;
+    let s2 = 'palmeiras' + i;
+    let result = '';
+    let x = 0;
+    if (s1.length < s2.length) {
+        while (x < s1.length) {
+            result += s1[x];
+            result += s2[x];
+            x += 1;
+        }
+        result += s2.substr(x);
+    } else {
+        while (x < s2.length) {
+            result += s1[x];
+            result += s2[x];
+            x += 1;
+        }
+        result += s1.substr(x);
+    }
+}
+
+
+console.timeEnd('perf');
