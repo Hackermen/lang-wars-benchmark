a174e6b4390f9aa67cdcaf51ff9b53559e46c7c6
Andrei Regiani <andrei.cpp@gmail.com>
2017-06-07 21:21:42 +0300

    Improved C++ solution, 60% better performance

--- round-1/solution-3.cpp
+++ round-1/solution-3.cpp
@@ -1,32 +1,43 @@
 #include <string>
-#include <iostream>
+#include <iostream> // DEBUG
 using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 10000000; i++) {
-        string s1 = "abacate" + to_string(i);
-        string s2 = "palmeiras" + to_string(i);
-        string result;
-        int x = 0;
+    string s1_base = "abacate";
+    string s2_base = "palmeiras";
+    string result;
+    string i_str;
+    int x;
 
-        if (s1.size() < s2.size()) {
-            while (x < s1.size()) {
-                result += s1[x];
-                result += s2[x];
+    for (int i = 0; i < 10000000; i++) {
+        i_str = to_string(i);
+        string s1 = s1_base + i_str;
+        string s2 = s2_base + i_str;
+
+        int s1_size = s1.size();
+        int s2_size = s2.size();
+
+        result = "";
+        x = 0;
+
+        if (s1_size < s2_size) {
+            while (x < s1_size) {
+                result += s1[x] + s2[x];
                 x++;
             }
             result += s2.substr(x);
         }
 
         else {
-            while (x < s2.size()) {
-                result += s1[x];
-                result += s2[x];
+            while (x < s2_size) {
+                result += s1[x] + s2[x];
                 x++;
             }
             result += s1.substr(x);
         }
+
+        // cout << result << endl; // DEBUG
     }
 
     return 0;
