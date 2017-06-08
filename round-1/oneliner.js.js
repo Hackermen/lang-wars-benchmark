5ad4b5c51508c7992a8e246b83a1cf5758e1f7a9
Andrei Regiani <andrei.cpp@gmail.com>
2017-06-08 18:02:49 +0300

    Clean up, organized by folder and renamed scripts

--- round-1/oneliner.js.js
+++
@@ -1,8 +0,0 @@
-const solvehacky = (str1,str2)=>str1.split('').map((c,i)=>c+(str2[i]||'')).join('')+str2.slice(str1.length);
-console.time ('perf');
-for (var i=10000000; i--;) {
-   let str1 = 'abacate' + i
-   let str2 = 'palmeiras' + i
-   let result = solvehacky(str1,str2)
- }
-console.timeEnd ('perf');
