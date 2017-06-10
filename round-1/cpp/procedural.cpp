ecf186283e24acc79d2dadc66e5c9733169fb9a3
Andrei Regiani <andrei.cpp@gmail.com>
2017-06-10 06:08:35 +0300

    Improved C++ implementation

--- round-1/cpp/procedural.cpp
+++ round-1/cpp/procedural.cpp
@@ -1,30 +1,31 @@
 #include <string>
 #include <iostream>
-using namespace std;
+
+// faster integer to string conversion
+void fast_cast(int source, char dest[]);
 
 int main()
 {
-    string s1_base = "ABCDEFH";
-    string s2_base = "123456789";
-    string result;
-    string i_str;
-    int x;
-    int s1_size;
-    int s2_size;
+    const std::string s1_base = "ABCDEFH";
+    const std::string s2_base = "123456789";
+    std::string result;
+    char i_str[9];
 
-    for (int i = 0; i < 10000000; i++) {
-        i_str = to_string(i);
-        string s1 = s1_base + i_str;
-        string s2 = s2_base + i_str;
-        s1_size = s1.size();
-        s2_size = s2.size();
+    for (int i = 0; i < 10'000'000; i++) {
+        fast_cast(i, i_str);
+
+        const std::string s1 = s1_base + i_str;
+        const std::string s2 = s2_base + i_str;
+        const int s1_size = s1.size();
+        const int s2_size = s2.size();
+
         result = "";
-        x = 0;
+        int x = 0;
 
         if (s1_size < s2_size) {
             while (x < s1_size) {
-                result += s1[x];
-                result += s2[x];
+                result.push_back(s1[x]);
+                result.push_back(s2[x]);
                 x++;
             }
             result += s2.substr(x);
@@ -32,15 +33,31 @@ int main()
 
         else {
             while (x < s2_size) {
-                result += s1[x];
-                result += s2[x];
+                result.push_back(s1[x]);
+                result.push_back(s2[x]);
                 x++;
             }
             result += s1.substr(x);
         }
 
-        if (i == 0) cout << result << endl;
+        if (i == 0) std::cout << result << std::endl;
     }
 
     return 0;
+}
+
+void fast_cast(int source, char dest[])
+{
+    char* p = dest;
+    char const digit[] = "0123456789";
+    int shifter = source;
+    do {
+        ++p;
+        shifter = shifter / 10;
+    } while (shifter);
+    *p = '\0';
+    do {
+        *--p = digit[source % 10];
+        source = source / 10;
+    } while(source);
 }
